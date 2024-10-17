a2ac97ec95b7894b744e3b912dead4024715a2db
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-10-17 14:19:57 +0900

    修正

--- projects/resources/shader/Object3d.PS.hlsl
+++ projects/resources/shader/Object3d.PS.hlsl
@@ -31,27 +31,31 @@ PixelShaderOutput main(VertexShaderOutput input)
     float4 transformedUV = mul(float32_t4(input.texcord, 0.0f, 1.0f), gMaterial.uvTransform);
     float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
     
+    //textureのα値が0のときにPixelを棄却
+    if (textureColor.a <= 0.0)
+    {
+        discard;
+    }
+    
     if (gMaterial.enableLighting != 0)
     {
-        //textureのα値が0のときにPixelを棄却
-        if (textureColor.a <= 0.0)
-        {
-            discard;
-        }
+        
         //half lambert
         float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
         float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
         output.color.a = gMaterial.color.a * textureColor.a;
         //output.colorのα値が0のときPixelを棄却
-        if (output.color.a == 0.0)
-        {
-            discard;
-        }
+        
     }
     else
     {
         output.color = gMaterial.color * textureColor;
+    }
+    
+    if (output.color.a == 0.0)
+    {
+        discard;
     }
     return output;
 }
