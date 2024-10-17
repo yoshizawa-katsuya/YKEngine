5016388e4e93c1a2d4fcdd0224f5579b575a5d53
takuma <k023g0015@g.neec.ac.jp>
2024-10-17 14:14:14 +0900

    デモプレイ会用のマップ描画

--- projects/Game/scene/GameScene.h
+++ projects/Game/scene/GameScene.h
@@ -48,7 +48,14 @@ private:
 
 	std::unique_ptr<MapChipField> mapChipField_;
 
+	//壁
 	std::vector<std::vector<std::unique_ptr<WorldTransform>>> worldTransformBlocks_;
+	//床
+	std::vector<std::vector<std::unique_ptr<WorldTransform>>> worldTransformFloors_;
+	//ばね
+	std::vector<std::vector<std::unique_ptr<WorldTransform>>> worldTransformSpring_;
+	//棘
+
 
 	std::unique_ptr<Camera> camera_;
 	std::unique_ptr<Camera> camera2_;
@@ -64,6 +71,7 @@ private:
 
 	std::unique_ptr<Model> modelPlayer_;
 	std::unique_ptr<Model> modelBlock_;
+	std::unique_ptr<Model> modelFloor_;
 
 	//プレイヤー
 	std::unique_ptr<Player> player_;
