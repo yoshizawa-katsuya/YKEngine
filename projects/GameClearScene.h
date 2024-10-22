b9ceefb6bf894d4fca4155c7a51ff8f6caf8e69e
PARK_YIJUN <tnswo2445@gmail.com>
2024-10-22 22:58:48 +0900

    gameOver,Clear

--- projects/GameClearScene.h
+++ projects/GameClearScene.h
@@ -29,5 +29,17 @@ private:
 
 	SpritePlatform* spritePlatform_;
 	ModelPlatform* modelPlatform_;
+
+	uint32_t gameClear1Sprite_ = 0;
+
+	std::unique_ptr<Sprite> gameClear1_ = nullptr;
+
+	uint32_t  gameClear2Sprite_ = 0;
+
+	std::unique_ptr<Sprite> gameClear2_ = nullptr;
+
+	int time = 40;
+
+
 };
 
