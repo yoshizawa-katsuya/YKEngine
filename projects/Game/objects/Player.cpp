0d81e7970e0c837615c8cb3185e2c457520ce109
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2024-12-11 12:32:04 +0900

    自キャラの左右移動を実装

--- projects/Game/objects/Player.cpp
+++ projects/Game/objects/Player.cpp
@@ -2,17 +2,36 @@
 #include "imgui/imgui.h"
 #include "Rigid3dObject.h"
 
+Player::Player()
+	: input_(Input::GetInstance())
+	, speed_(0.1f)
+	, lrDirection_(LRDirection::kRight)
+{
+}
+
 void Player::Initialize(BaseModel* model) {
 
 	object_ = std::make_unique<Rigid3dObject>();
 	object_->Initialize(model);
 
 	worldTransform_.Initialize();
-
+	worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
 }
 
 void Player::Update() {
 
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.translation_.x -= speed_;
+		if (lrDirection_ != LRDirection::kLeft) {
+			lrDirection_ = LRDirection::kRight;
+		}
+	}
+	if (input_->PushKey(DIK_D)) {
+		worldTransform_.translation_.x += speed_;
+		if (lrDirection_ != LRDirection::kRight) {
+			lrDirection_ = LRDirection::kLeft;
+		}
+	}
 
 #ifdef _DEBUG
 
--- projects/Game/objects/Player.h
+++ projects/Game/objects/Player.h
@@ -2,12 +2,16 @@
 #include "Base3dObject.h"
 #include "WorldTransform.h"
 #include "Animation.h"
+#include "Input.h"
+#include <numbers>
 class Camera;
 class MapChipField;
 
 class Player
 {
 public:
+
+	Player();
 
 	void Initialize(BaseModel* model);
 
@@ -21,6 +25,19 @@ private:
 	WorldTransform worldTransform_;
 
 	std::unique_ptr<Base3dObject> object_;
+
+	Input* input_;
+
+	enum  class LRDirection {
+		kRight,
+		kLeft,
+	};
+
+	LRDirection lrDirection_;
+	// 左右の自キャラ角度テーブル
+	std::vector<float> destinationRotationYTable = { std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> *3.0f / 2.0f };
+
+	float speed_;
 
 };
 
