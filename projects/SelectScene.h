1294e0e0991600615eeacc64bdbf64747466f676
PARK_YIJUN <tnswo2445@gmail.com>
2025-02-03 13:52:01 +0900

    move stone

--- projects/SelectScene.h
+++ projects/SelectScene.h
@@ -28,7 +28,7 @@ public:
 
 	uint32_t GetSelectedTutorial() const;
 	uint32_t GetSelectedBundle() const;
-	uint32_t GetSelectedStage() const;
+	
 
 
 private:
@@ -61,36 +61,25 @@ private:
 		kRightSide,
 	};
 	std::array<std::unique_ptr<Sprite>, 3> selectSprites_;
-	//bundle 7
-	std::vector<uint32_t> bundleTextures_;
-	std::vector<std::unique_ptr<Sprite>> bundleSprites_;
-	//selectStage 10
-	std::vector<uint32_t> stageTextures_;
-	std::vector<std::unique_ptr<Sprite>> stageSprites_;
+	
 	//
 	std::vector<uint32_t> difficulty_;
 	std::vector<std::unique_ptr<Sprite>> difficultySprites_;
 
-	//return Button
-	uint32_t reButton1_;
-	std::unique_ptr<Sprite> reButtonSprite1_;
-	uint32_t reButton2_;
-	std::unique_ptr<Sprite> reButtonSprite2_;
-	uint32_t reButton3_;
-	std::unique_ptr<Sprite> reButtonSprite3_;
+	
 
-	std::vector<bool> bundleActive_;
+	
 	bool selectSpr = true;
 
 	bool selectClick1 = true;
 
-	bool isAnyBundleActive_ = false;
+	
 
 	bool selectClick2 = false;
 
 	// Save selected elements
 	uint32_t selectedTutorial_ = 0; 
 	uint32_t selectedBundle_ = 0;   
-	uint32_t selectedStage_ = 0;
+	
 
 };
