b99914149917071114f1eda3e874946be9624ca2
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-02-04 14:17:30 +0900

    石の衝突判定実装途中

--- projects/Game/objects/Stone.h
+++ projects/Game/objects/Stone.h
@@ -9,7 +9,7 @@ public:
 
 	Stone();
 
-	void Initialize(BaseModel* model, const Vector3& position);
+	void Initialize(BaseModel* model, const Vector3& position, MapChipField* mapChipField);
 
 	void Update();
 
@@ -23,11 +23,42 @@ public:
 
 private:
 
+	// マップとの当たり判定情報
+	struct CollisionMapInfo {
+		//手前か奥の衝突判定
+		bool isWallCollisionDepth = false;
+		//左右の衝突判定
+		bool isWallCollisionWidth = false;
+		Vector3 move;
+	};
+
+	enum Corner {
+		kRightFront,	//右手前
+		kLeftFront,		//左手前
+		kRightBack,		//右奥
+		kLeftBack,		//左奥
+
+		kNumCorner		//要素数
+
+	};
+
 	//移動入力
 	void Move();
 
-	//移動
+	//マップ衝突判定
+	void MapCollision();
+	void MapCollisionBack();
+	void MapCollisionFront();
+	void MapCollisionRight();
+	void MapCollisionLeft();
+
+	//判定結果を反映して移動させる
 	void MoveAppli();
+
+	//壁に接触している場合の処理
+	void WallCollisionDepth();
+
+	Vector3 CornerPosition(const Vector3& center, Corner corner);
 
 	Vector3 ConvertScreenToWorld(const Vector2& screenPos);
 
@@ -44,5 +75,14 @@ private:
 
 	//マップチップによるフィールド
 	MapChipField* mapChipField_ = nullptr;
+
+	CollisionMapInfo collisionMapInfo_;
+
+	//キャラクターの当たり判定サイズ
+	const float kWidth = 1.0f;
+	const float kDepth = 1.0f;
+
+	//余白
+	const float kBlank = 0.01f;
 };
 
