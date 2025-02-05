820b152381ff0e8539054e21e8ca60908cc4d3b9
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-02-05 16:59:52 +0900

    衝突判定実装

--- projects/Game/objects/Stone.cpp
+++ projects/Game/objects/Stone.cpp
@@ -34,7 +34,7 @@ void Stone::Update()
 	MoveAppli();
 
 	WallCollisionDepth();
-
+	WallCollisionWidth();
 }
 
 void Stone::Draw(Camera* camera)
@@ -101,19 +101,19 @@ void Stone::MapCollisionBack()
 	}
 
 	MapChipType mapChipType;
-	//真上の当たり判定を行う
+	//奥の当たり判定を行う
 	bool hit = false;
-	//左上点の判定
+	//左奥点の判定
 	MapChipField::IndexSet indexSet;
 	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kLeftBack]);
 	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
-	if (mapChipType == MapChipType::kBox) {
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
 		hit = true;
 	}
-	//右上点の判定
+	//右奥点の判定
 	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightBack]);
 	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
-	if (mapChipType == MapChipType::kBox) {
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
 		hit = true;
 	}
 
@@ -122,8 +122,8 @@ void Stone::MapCollisionBack()
 		indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightBack]);
 		//めり込み先ブロックの範囲矩形
 		MapChipField::Rect rect = mapChipField_->GetRectByIndex(indexSet.xIndex, indexSet.zIndex);
-		collisionMapInfo_.move.y = (std::max)(0.0f, (rect.front - worldTransform_.translation_.y) - (kDepth / 2 + kBlank));
-		//天井に当たったことを記録する
+		collisionMapInfo_.move.z = (std::max)(0.0f, (rect.front - worldTransform_.translation_.z) - (kDepth / 2 + kBlank));
+		//壁に当たったことを記録する
 		collisionMapInfo_.isWallCollisionDepth = true;
 	}
 
@@ -131,19 +131,145 @@ void Stone::MapCollisionBack()
 
 void Stone::MapCollisionFront()
 {
+	if (collisionMapInfo_.move.z >= 0)
+	{
+		return;
+	}
+
+	//移動後の4つの角の座標
+	std::array<Vector3, kNumCorner> positionsNew;
+
+	for (uint32_t i = 0; i < positionsNew.size(); ++i)
+	{
+		positionsNew[i] = CornerPosition(
+			{ worldTransform_.translation_.x + collisionMapInfo_.move.x, worldTransform_.translation_.y + collisionMapInfo_.move.y, worldTransform_.translation_.z + collisionMapInfo_.move.z }, static_cast<Corner>(i));
+	}
+
+	MapChipType mapChipType;
+	//手前の当たり判定を行う
+	bool hit = false;
+	//左手前点の判定
+	MapChipField::IndexSet indexSet;
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kLeftFront]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+	//右手前点の判定
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightFront]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+
+	if (hit) {
+		//めり込みを排除する方向に移動量を設定する
+		indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightFront]);
+		//めり込み先ブロックの範囲矩形
+		MapChipField::Rect rect = mapChipField_->GetRectByIndex(indexSet.xIndex, indexSet.zIndex);
+		collisionMapInfo_.move.z = (std::min)(0.0f, (rect.back - worldTransform_.translation_.z) + (kDepth / 2 + kBlank));
+		//壁に当たったことを記録する
+		collisionMapInfo_.isWallCollisionDepth = true;
+	}
+
 }
 
 void Stone::MapCollisionRight()
 {
+	if (collisionMapInfo_.move.x <= 0)
+	{
+		return;
+	}
+
+	//移動後の4つの角の座標
+	std::array<Vector3, kNumCorner> positionsNew;
+
+	for (uint32_t i = 0; i < positionsNew.size(); ++i)
+	{
+		positionsNew[i] = CornerPosition(
+			{ worldTransform_.translation_.x + collisionMapInfo_.move.x, worldTransform_.translation_.y + collisionMapInfo_.move.y, worldTransform_.translation_.z + collisionMapInfo_.move.z }, static_cast<Corner>(i));
+	}
+
+	MapChipType mapChipType;
+	//奥の当たり判定を行う
+	bool hit = false;
+	//左奥点の判定
+	MapChipField::IndexSet indexSet;
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightFront]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+	//右奥点の判定
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightBack]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+
+	if (hit) {
+		//めり込みを排除する方向に移動量を設定する
+		indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kRightBack]);
+		//めり込み先ブロックの範囲矩形
+		MapChipField::Rect rect = mapChipField_->GetRectByIndex(indexSet.xIndex, indexSet.zIndex);
+		collisionMapInfo_.move.x = (std::max)(0.0f, (rect.left - worldTransform_.translation_.x) - (kWidth / 2 + kBlank));
+		//壁に当たったことを記録する
+		collisionMapInfo_.isWallCollisionWidth = true;
+	}
+
 }
 
 void Stone::MapCollisionLeft()
 {
+	if (collisionMapInfo_.move.x >= 0)
+	{
+		return;
+	}
+
+	//移動後の4つの角の座標
+	std::array<Vector3, kNumCorner> positionsNew;
+
+	for (uint32_t i = 0; i < positionsNew.size(); ++i)
+	{
+		positionsNew[i] = CornerPosition(
+			{ worldTransform_.translation_.x + collisionMapInfo_.move.x, worldTransform_.translation_.y + collisionMapInfo_.move.y, worldTransform_.translation_.z + collisionMapInfo_.move.z }, static_cast<Corner>(i));
+	}
+
+	MapChipType mapChipType;
+	//手前の当たり判定を行う
+	bool hit = false;
+	//左手前点の判定
+	MapChipField::IndexSet indexSet;
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kLeftFront]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+	//右手前点の判定
+	indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kLeftBack]);
+	mapChipType = mapChipField_->GetMapChipTypeByIndex(indexSet.xIndex, indexSet.zIndex);
+	if (mapChipType == MapChipType::kBox || mapChipType == MapChipType::ice) {
+		hit = true;
+	}
+
+	if (hit) {
+		//めり込みを排除する方向に移動量を設定する
+		indexSet = mapChipField_->GetMapChipIndexSetByPosition(positionsNew[kLeftBack]);
+		//めり込み先ブロックの範囲矩形
+		MapChipField::Rect rect = mapChipField_->GetRectByIndex(indexSet.xIndex, indexSet.zIndex);
+		collisionMapInfo_.move.x = (std::min)(0.0f, (rect.right - worldTransform_.translation_.x) + (kDepth / 2 + kBlank));
+		//壁に当たったことを記録する
+		collisionMapInfo_.isWallCollisionWidth = true;
+	}
+
 }
 
 void Stone::MoveAppli()
 {
-	worldTransform_.translation_ += velocity_;
+	if (collisionMapInfo_.isWallCollisionWidth) {
+		collisionMapInfo_.move.z = velocity_.z;
+	}
+	worldTransform_.translation_ += collisionMapInfo_.move;
 	velocity_ *= friction_;
 
 	if (fabs(velocity_.x) < 0.01f) velocity_.x = 0.0f;
@@ -156,18 +282,25 @@ void Stone::MoveAppli()
 
 void Stone::WallCollisionDepth()
 {
-	if (collisionMapInfo_.isWallCollisionDepth) {
+	if (collisionMapInfo_.isWallCollisionDepth && !collisionMapInfo_.isWallCollisionWidth) {
 		velocity_.z = -velocity_.z;
+	}
+}
+
+void Stone::WallCollisionWidth()
+{
+	if (collisionMapInfo_.isWallCollisionWidth) {
+		velocity_.x = -velocity_.x;
 	}
 }
 
 Vector3 Stone::CornerPosition(const Vector3& center, Corner corner)
 {
 	Vector3 offsetTable[kNumCorner] = {
-		{+kWidth / 2.0f, -kDepth / 2.0f, 0},	//kRightFront
-		{-kWidth / 2.0f, -kDepth / 2.0f, 0},	//kLeftFront
-		{+kWidth / 2.0f, +kDepth / 2.0f, 0},	//kRightBack
-		{-kWidth / 2.0f, +kDepth / 2.0f, 0},	//kLeftBack
+		{+kWidth / 2.0f, 0.0f, -kDepth / 2.0f},	//kRightFront
+		{-kWidth / 2.0f, 0.0f, -kDepth / 2.0f},	//kLeftFront
+		{+kWidth / 2.0f, 0.0f, +kDepth / 2.0f},	//kRightBack
+		{-kWidth / 2.0f, 0.0f, +kDepth / 2.0f},	//kLeftBack
 	};
 
 	return { center.x + offsetTable[static_cast<uint32_t>(corner)].x,
