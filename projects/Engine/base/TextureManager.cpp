427684f61e9dd9f621fa8ebe8314d8d2d858f3b4
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-04-23 16:20:04 +0900

    修正

--- projects/Engine/base/TextureManager.cpp
+++ projects/Engine/base/TextureManager.cpp
@@ -153,6 +153,8 @@ void TextureManager::LoadTexture(const std::string& filePath, uint32_t index) {
 [[nodiscard]]
 Microsoft::WRL::ComPtr<ID3D12Resource> TextureManager::UploadTextureData(ID3D12Resource* textureResource, const DirectX::ScratchImage& mipImages)
 {
+	std::unique_lock<std::mutex> lock(mutex_);
+
 	std::vector<D3D12_SUBRESOURCE_DATA> subresources;
 	ID3D12Device* device = dxCommon_->GetDevice();
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
