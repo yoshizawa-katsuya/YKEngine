b9a7dbec8e8dc26a3917b069ca444ea9848a7a35
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-05-30 14:58:27 +0900

    FaceToVelocityDirectionを修正

--- projects/Engine/lib/TransformHelpers.cpp
+++ projects/Engine/lib/TransformHelpers.cpp
@@ -12,11 +12,11 @@ Vector3 TransformHelpers::FaceToVelocityDirection(const Vector3& rotate, const V
 	}
 	// Y軸回り角度(θy)
 	Vector3 newRotate = rotate;
-	newRotate.y = std::atan2(velocity.x, velocity.z) + std::numbers::pi_v<float>;
+	newRotate.y = std::atan2(velocity.x, velocity.z);
 	Matrix4x4 minusThetaY = MakeRotateYMatrix(-newRotate.y);
 	Vector3 velocityZ = Transform(velocity, minusThetaY);
 	//X軸回り角度(θx)
-	newRotate.x = std::atan2(-velocityZ.y, -velocityZ.z);
+	newRotate.x = std::atan2(-velocityZ.y, velocityZ.z);
 
 	return newRotate;
 }
