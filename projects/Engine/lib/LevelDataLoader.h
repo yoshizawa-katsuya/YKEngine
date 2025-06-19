9c51d0c2a82f91f3d2fcd3cb00a8901a82a8694c
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-06-19 16:42:49 +0900

    jsonから曲線の制御点の情報を読み込めるように変更

--- projects/Engine/lib/LevelDataLoader.h
+++ projects/Engine/lib/LevelDataLoader.h
@@ -14,12 +14,21 @@ struct PlayerSpawnData
 	EulerTransform transform;
 };
 
+//スプライン曲線の制御点データ
+struct SplineData
+{
+	std::list<Vector3> controlPoints;
+};
+
 struct LevelData
 {
 	std::list<ObjectData> objects;
 
 	//自キャラ配列
 	std::list<PlayerSpawnData> playerSpawns;
+
+	//スプライン曲線の制御点データ
+	std::list<SplineData> splines;
 };
 
 LevelData* LevelDataLoad(const std::string& kDefaultBaseDirectory, const std::string& fileName, const std::string& kExtension);
