c4f8e1acbac11d522025cdbe9fb96f9d575804e8
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-08-04 17:53:30 +0900

    Vector2のoperatorを追加

--- projects/Engine/lib/Vector2.h
+++ projects/Engine/lib/Vector2.h
@@ -3,6 +3,11 @@
 struct Vector2 {
 	float x;
 	float y;
+
+	Vector2& operator*=(float s) { x *= s;  y *= s; return *this; }
+	Vector2& operator-=(const Vector3& v) { x -= v.x; y -= v.y; return *this; }
+	Vector2& operator+=(const Vector3& v) { x += v.x; y += v.y; return *this; }
+	Vector2& operator/=(float s) { x /= s;  y /= s; return *this; }
 };
 
 //加算
