de7a8b1d24ecde40787dd9b817ff97be6e3df09e
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-10-30 15:01:19 +0900

    translate用のSlerp関数を実装

--- projects/Engine/lib/Slerp.h
+++ projects/Engine/lib/Slerp.h
@@ -5,5 +5,8 @@
 //球面線形補完
 Vector3 Slerp(const Vector3& v1, const Vector3& v2, float t);
 
+//球面線形補完(位置ベクトル版)
+Vector3 SlerpTranslate(const Vector3& start, const Vector3& end, float t);
+
 //球面線形補完(クォータニオン版)
 Quaternion Slerp(const Quaternion& v1, const Quaternion& v2, float t);
