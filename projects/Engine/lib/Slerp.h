255e05efdc86a461b39f1c9fb2eba39d9ce81853
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-10-31 16:16:17 +0900

    Slerp関数を追加

--- projects/Engine/lib/Slerp.h
+++ projects/Engine/lib/Slerp.h
@@ -9,10 +9,12 @@ Vector3 Slerp(const Vector3& v1, const Vector3& v2, float t);
 Vector3 SlerpTranslate(const Vector3& start, const Vector3& end, float t);
 
 //球面線形補完(座標の中心を基準にした位置ベクトル版)
-Vector3 SlerpTranslteByCenter(const Vector3& start, const Vector3& end, float t);
+Vector3 SlerpTranslateByCenter(const Vector3& start, const Vector3& end, float t);
 
 //球面線形補完(座標の中心を基準にした位置ベクトル版、中心座標指定可能)
-Vector3 SlerpTranslteByCenter(const Vector3& center, const Vector3& start, const Vector3& end, float t);
+Vector3 SlerpTranslateByCenter(const Vector3& center, const Vector3& start, const Vector3& end, float t);
+
+Vector3 SlerpTranslateByCenterAxis(const Vector3& center, const Vector3& axis, const Vector3& start, const Vector3& end, float t);
 
 //球面線形補完(クォータニオン版)
 Quaternion Slerp(const Quaternion& v1, const Quaternion& v2, float t);
