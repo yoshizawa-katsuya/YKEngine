b9745ff8ec48c31f836509a9c7991c5465b41b3e
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-11-28 17:54:18 +0900

    Engineとマージ

--- projects/Game/scene/SceneFactory.cpp
+++ projects/Game/scene/SceneFactory.cpp
@@ -16,10 +16,10 @@ std::unique_ptr<BaseScene> SceneFactory::CreateScene(const std::string& sceneNam
 		newScene = std::make_unique<GameScene>();
 	}
 	else if (sceneName == "GameOverScene") {
-		newScene = new GameOverScene();
+		newScene = std::make_unique<GameOverScene>();
 	}
 	else if (sceneName == "ClearScene") {
-		newScene = new ClearScene();
+		newScene = std::make_unique<ClearScene>();
 	}
 	
 
