b9667badd29cbb2ebe3a2493f318b16b06710c03
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2025-12-09 12:58:11 +0900

    関数を追加

--- projects/Engine/3d/BaseModel.cpp
+++ projects/Engine/3d/BaseModel.cpp
@@ -126,6 +126,12 @@ void BaseModel::SetEnableLighting(bool enableLighting)
 	materialData_->enableLighting = enableLighting;
 }
 
+void YKEngine::BaseModel::SetShininess(float shininess)
+{
+	ThreadPool::GetInstance()->waitForCompletion();
+	materialData_->shininess = shininess;
+}
+
 void BaseModel::SetEnvironmentCoefficient(float environmentCoefficient)
 {
 	ThreadPool::GetInstance()->waitForCompletion();
