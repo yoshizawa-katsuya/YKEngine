39cb50f03d1dda222cedff6fa5745848cf601a2a
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2026-05-29 10:31:08 +0900

    壁のポーズごとにモデルが切り替わるように実装

--- projects/Game/objects/WallModels.cpp
+++ projects/Game/objects/WallModels.cpp
@@ -7,10 +7,10 @@ void WallModels::Initialize()
 {
 	//壁のモデルの生成
 	ModelPlatform* modelPlatform = ModelPlatform::GetInstance();
-	wallModels_[PlayerPose::A] = modelPlatform->CreateRigidModel("./resources/wall", "APoseWall.obj");
-	wallModels_[PlayerPose::B] = modelPlatform->CreateRigidModel("./resources/wall", "BPoseWall.obj");
-	wallModels_[PlayerPose::C] = modelPlatform->CreateRigidModel("./resources/wall", "CPoseWall.obj");
-	wallModels_[PlayerPose::D] = modelPlatform->CreateRigidModel("./resources/wall", "DPoseWall.obj");
-	wallModels_[PlayerPose::Squat] = modelPlatform->CreateRigidModel("./resources/wall", "SquatWall.obj");
-	wallModels_[PlayerPose::Base] = modelPlatform->CreateRigidModel("./resources/wall", "BaseWall.obj");
+	wallModels_[PlayerPose::A] = modelPlatform->CreateRigidModel("./Resources/walls", "PoseAWall.obj");
+	wallModels_[PlayerPose::B] = modelPlatform->CreateRigidModel("./Resources/walls", "PoseBWall.obj");
+	wallModels_[PlayerPose::C] = modelPlatform->CreateRigidModel("./Resources/walls", "PoseCWall.obj");
+	wallModels_[PlayerPose::D] = modelPlatform->CreateRigidModel("./Resources/walls", "PoseDWall.obj");
+	wallModels_[PlayerPose::Squat] = modelPlatform->CreateRigidModel("./Resources/walls", "SquatWall.obj");
+	wallModels_[PlayerPose::Base] = modelPlatform->CreateRigidModel("./Resources/wall", "wall.obj");
 }
