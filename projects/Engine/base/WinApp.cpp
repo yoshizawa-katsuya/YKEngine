6a5ee66f0845358a4f61db7f777325ef63c26a1b
yoshizawa-katsuya <146906478+yoshizawa-katsuya@users.noreply.github.com>
2026-09-09 14:38:53 +0900

    タイトルバーを変更

--- projects/Engine/base/WinApp.cpp
+++ projects/Engine/base/WinApp.cpp
@@ -81,7 +81,7 @@ void WinApp::CreateGameWindow() {
 	//ウィンドウの生成
 	hwnd_ = CreateWindow(
 		wc_.lpszClassName,		//利用するクラス名	
-		L"LINK:OVER",			//タイトルバーの文字(何でも良い)
+		L"4028_LINK:OVER",			//タイトルバーの文字(何でも良い)
 		WS_OVERLAPPEDWINDOW,	//よく見るウィンドウスタイル
 		CW_USEDEFAULT,			//表示x座標(Windowsに任せる)
 		CW_USEDEFAULT,			//表示y座標(WindowsOSに任せる)
